b76f686345c19c2b8d896f1c9206c652b9b505c8
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2020-03-08 23:27:31 +0100

    added github links to projects

--- pagecontent/index.php
+++ pagecontent/index.php
@@ -2,7 +2,7 @@
 
     require_once("template_classes/template_page.php");
 
-    $t = new TEMPLATE_PAGE ("0.1.1", "BSERG", "Under Development", "img/projects/bserg.png", array("Website"));
+    $t = new TEMPLATE_PAGE ("0.1.1", "BSERG", "Under Development", "img/projects/bserg.png", array("Website"),  array(""), "https://github.com/Joni1234321/robotarmprojekt");
     $t->create_p("PERSONAL WEBSITE <br> Property of Jonas Vendelbo Hansen");
     $t->create_img("img/car.jpg", "profilepicture", "CAR");
     $t->create_p("THIS WEBSITES CONTAINS PROJECTS MADE BY SAID PERSON");
